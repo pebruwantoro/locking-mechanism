dcdf1f248984c4abfb1246f3d19b3e6cfb96ba7e
pebruwantoro <pebruwantoro.doni@gmail.com>
2025-06-27 09:47:05 +0700

    optimistic-locking

---
+++ optimistic/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/logger"
+	"gorm.io/plugin/optimisticlock"
+)
+
+type Product struct {
+	ID    uint `grom:"primaryKey"`
+	Name  string
+	Stock int64
+	// optimistic purpose
+	Version optimisticlock.Version
+}
+
+func setupDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=database port=5433 sslmode=disable"
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		},
+	)
+	if err != nil {
+		log.Fatal("Failed to connect database")
+	}
+
+	err = db.AutoMigrate(&Product{})
+	if err != nil {
+		log.Fatal("Failed run migration")
+	}
+
+	return db
+}
+
+func processOrderPessimistic(db *gorm.DB, productID uint, quantity int64, wg *sync.WaitGroup, workerID int) {
+	defer wg.Done()
+
+	err := db.Transaction(func(tx *gorm.DB) error {
+		var product Product
+
+		if err := tx.Clauses(clause.Locking{
+			Strength: "UPDATE",
+		}).First(&product, productID).Error; err != nil {
+			return err
+		}
+
+		fmt.Printf("Acquired lock. Worker: %d, Current stock: %d\n", workerID, product.Stock)
+		time.Sleep(100 * time.Millisecond)
+
+		if product.Stock < quantity {
+			return fmt.Errorf("worker %d: not enough stock", workerID)
+		}
+
+		product.Stock -= quantity
+		if err := tx.Save(&product).Error; err != nil {
+			return err
+		}
+
+		fmt.Printf("Worker %d: Oder processed. New stock: %d\n", workerID, product.Stock)
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Transaction failed: %v, worker: %d", err, workerID)
+	}
+}
+
+func main() {
+	db := setupDB()
+
+	db.Exec("TRUNCATE TABLE products RESTART IDENTITY")
+	product := Product{Name: "Sugar", Stock: 10}
+	db.Create(&product)
+
+	fmt.Println("Running Optimistic Locking Example")
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 20; i++ {
+		wg.Add(1)
+		go processOrderPessimistic(db, product.ID, 1, &wg, i)
+	}
+
+	wg.Wait()
+	fmt.Println("All workers finished")
+}
